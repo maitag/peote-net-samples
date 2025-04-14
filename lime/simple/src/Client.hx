40971a02a1d073816fba08d7d768628cf4db3950
maitag <info@maitag.de>
2025-04-15 00:31:23 +0200

    put err-msg-parsing in and some simple ping-pong

--- lime/simple/src/Client.hx
+++ lime/simple/src/Client.hx
@@ -1,13 +1,18 @@
 package;
 
+import haxe.Timer;
 import haxe.io.Bytes;
 import peote.net.PeoteClient;
+import peote.net.Reason;
 
 class Client extends lime.app.Application
 {
 	var host:String = "localhost";
 	var port:Int = 7680;
+
 	var channelName = "testChannel";
+
+	var peoteClient:PeoteClient;
 	
 	public function new()
 	{
@@ -15,28 +20,34 @@ class Client extends lime.app.Application
 		
 		trace("----------- START CLIENT -----------");
 		
-		var peoteClient = new PeoteClient(
+		peoteClient = new PeoteClient(
 		{
-			onEnter: function(client:PeoteClient)
-			{
+			onEnter: function(client:PeoteClient) {
 				trace('CLIENT -> onEnterJoint: Joint number ${client.jointNr} entered');
 			},
 			
-			onData: function(client:PeoteClient, data:Bytes)
-			{
+			onData: function(client:PeoteClient, data:Bytes) {
 				trace('CLIENT -> onData: jointNr:${client.jointNr}, data:${data}');
+
+				Timer.delay(()-> {
+					peoteClient.send( Bytes.ofString("PING") );
+				}, 1000);		
 			},
 			
-			onDisconnect: function(client:PeoteClient, reason:Int)
-			{
+			onDisconnect: function(client:PeoteClient, reason:Int) {
 				trace('CLIENT -> onDisconnect: jointNr=${client.jointNr}');
 			},
 			
-			onError: function(client:PeoteClient, reason:Int)
-			{
-				trace('CLIENT -> onEnterJointError:$reason');
-			}
-			
+			onError: function(client:PeoteClient, reason:Int) {
+				switch(reason) {
+					case Reason.DISCONNECT:trace("CLIENT -> Can't connect to peote-server.");
+					case Reason.CLOSE:     trace("CLIENT -> Connection to peote-server is closed.");
+					case Reason.ID:        trace("CLIENT -> No channel with this ID to enter.");
+					case Reason.MAX:       trace("CLIENT -> Entered to much channels on this server at the same time (max is 128)");
+					case Reason.FULL:      trace("CLIENT -> Channel is full (max of 256 users already connected).");
+					case Reason.MALICIOUS: trace("CLIENT -> Malicious data.");
+				}
+			}			
 		});		
 		
 		// enter server
--- lime/simple/src/Server.hx
+++ lime/simple/src/Server.hx
@@ -1,13 +1,18 @@
 package;
 
+import haxe.Timer;
 import haxe.io.Bytes;
 import peote.net.PeoteServer;
+import peote.net.Reason;
 
 class Server extends lime.app.Application
 {	
 	var host:String = "localhost";
 	var port:Int = 7680;
+
 	var channelName = "testChannel";
+
+	var peoteServer:PeoteServer;
 	
 	public function new()
 	{
@@ -15,33 +20,42 @@ class Server extends lime.app.Application
 
 		trace("----------- START SERVER -----------");
 		
-		var peoteServer = new PeoteServer(
+		peoteServer = new PeoteServer(
 		{
-			onCreate: function(server:PeoteServer)
-			{
+			onCreate: function(server:PeoteServer) {
 				trace('SERVER -> onCreateJoint: Channel ${server.jointNr} created.');
 			},
 			
-			onUserConnect: function(server:PeoteServer, userNr:Int)
-			{
+			onUserConnect: function(server:PeoteServer, userNr:Int) {
 				trace('SERVER -> onUserConnect: jointNr:${server.jointNr}, userNr:$userNr');
+
+				peoteServer.send(userNr, Bytes.ofString("hello"));
+
+				// TODO: send to all connected users out of userNr itself
+				// peoteServer.broadcast( Bytes.ofString('userNr:$userNr connected to server'), userNr);
 			},
 			
-			onData: function(server:PeoteServer, userNr:Int, data:Bytes)
-			{
-				trace('SERVER -> onData: jointNr:${server.jointNr}, userNr:$userNr');				
+			onData: function(server:PeoteServer, userNr:Int, data:Bytes) {
+				trace('SERVER -> onData: jointNr:${server.jointNr}, userNr:$userNr, data:${data}');
+
+				Timer.delay(()-> {
+					peoteServer.send(userNr, Bytes.ofString("PONG"));
+				}, 1000);		
 			},
 			
-			onUserDisconnect: function(server:PeoteServer, userNr:Int, reason:Int)
-			{
+			onUserDisconnect: function(server:PeoteServer, userNr:Int, reason:Int) {
 				trace('SERVER -> onUserDisconnect: jointNr:${server.jointNr}, userNr:$userNr');
 			},
 			
-			onError: function(server:PeoteServer, userNr:Int, reason:Int)
-			{
-				trace('SERVER -> onCreateJointError:$reason, userNr:$userNr');
-			}
-			
+			onError: function(server:PeoteServer, userNr:Int, reason:Int) {
+				switch(reason) {
+					case Reason.DISCONNECT:trace("SERVER -> Can't connect to peote-server.");
+					case Reason.CLOSE:     trace("SERVER -> Connection to peote-server is closed.");
+					case Reason.ID:        trace("SERVER -> There is another channel with same ID. (or wrong ID)");
+					case Reason.MAX:       trace("SERVER -> Created to much channels on this server at the same time (max is 128).");
+					case Reason.MALICIOUS: trace('SERVER -> User $userNr sending malicious data.');
+				}
+			}			
 		});
 				
 		// create server
